5839d2048b961199cabc5b1a24b3d179c12977cb
Kapaya <kkatongo@mit.edu>
2015-04-15 00:21:50 -0400

    working on create album

--- javascripts/button_actions.js
+++ javascripts/button_actions.js
@@ -1,5 +1,5 @@
 $(document).on("click", "#register_button", function(){
-	window.location = "./profilepage.html"
+	window.location = "./blank_profile_page.html"
 });
 
 $(document).on("click", "#login_button", function(){
@@ -14,7 +14,14 @@ $(document).on("click", "#save_album_button", function(){
 	window.location = "./profilepage.html"
 });
 
-$(document).on("click", ".logout_button", function(){
-	window.location = "./showcase.html"
+$(document).on("click", "#cancel_album_button", function(){
+	window.location = "./blank_profile_page.html"
 });
 
+$(document).on("click", ".logout_button", function(){
+	window.location = "./index.html"
+});
+
+$(document).on("click", ".create_album", function(){
+	window.location = "./creating_album_page.html"
+});
