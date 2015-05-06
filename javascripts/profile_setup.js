6ccd1d0773f0ae2359137b8e7f2baabd516478cd
ermanzi <ermanzi@mit.edu>
2015-05-05 21:26:00 -0400

    minor changes

--- javascripts/profile_setup.js
+++ javascripts/profile_setup.js
@@ -161,8 +161,7 @@ $(document).on("click", "#createPortfolioButton", function(){
 								var photo = list[0];
 								photo.set("portfolio", portfolioId);
 								photo.save();
-								window.location = "./profile_page.html"; //add portfolio variables !*!
-
+								window.location = "./profile_page.html";
 							}
 						});
 					}); 
